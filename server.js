a9e7e79a9b9bd64fd813caefce65bf145fba6fab
mansuur-iman <imaninspires1@gmail.com>
2026-03-15 14:44:21 +0300

    added cors to the server

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ import express from "express";
 import userRouter from "./routes/userRouter.js";
 import postRouter from "./routes/postRouter.js";
 import commentRouter from "./routes/commentRouter.js";
+import cors from "cors";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
